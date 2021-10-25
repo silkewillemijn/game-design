7bb0c808ac4546e0befd34b0426cd329df1d5344
silkewillemijn <s.w.franken@student.tue.nl>
2021-10-25 10:34:47 +0200

    added sound effect vanashing, tried to add delay but doesn't work yet
    also probably fixed null reference error

--- Fantasy World/Assets/DaughterMovement.cs
+++ Fantasy World/Assets/DaughterMovement.cs
@@ -7,7 +7,6 @@ public class DaughterMovement : MonoBehaviour
     Animator animator;
     GameObject girl;
     GameObject player;
-    bool isTalking = false;
     public AudioSource playVoice1;
     
     // Start is called before the first frame update
@@ -21,18 +20,13 @@ public class DaughterMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var distance = Vector3.Distance(girl.transform.position, player.transform.position);
-        //Debug.Log(distance);
+        if (girl.activeInHierarchy == true) {
+            var distance = Vector3.Distance(girl.transform.position, player.transform.position);
 
-        //if player is at certain distance of girl, the trigger will be activated
-        if (distance < 10 && !isTalking) {
-            animator.SetTrigger("closeEnough");
-            // FindObjectOfType<AudioManager>().Play("GirlVoiceOne");
-            // isTalking = true;
+            //if player is at certain distance of girl, the trigger will be activated
+            if (distance < 10) {
+                animator.SetTrigger("closeEnough");
+            }
         }
-
-        // if (distance > 10) { // best is to look at state of audio: it will currently start playing again when you walk back and forth
-        //     isTalking = false;
-        // }
     }
 }
