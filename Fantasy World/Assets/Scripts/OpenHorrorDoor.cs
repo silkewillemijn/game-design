80254119c472eb5360d46e1200a864053bd52d39
silkewillemijn <s.w.franken@student.tue.nl>
2021-11-01 15:17:23 +0100

    tried adding some sounds in underwaterworld

--- Fantasy World/Assets/Scripts/OpenHorrorDoor.cs
+++ Fantasy World/Assets/Scripts/OpenHorrorDoor.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class OpenHorrorDoor : MonoBehaviour
 {
+    public AudioSource doorSound;
     // Start is called before the first frame update
     void Update()
     {
         if (DoorInteractor.hitDoor == true)
         {
             this.GetComponent<Animator>().SetBool("open", true); //open door
+            doorSound.Play();
+            if (doorSound.isPlaying  == false) {
+                doorSound.volume = 0;
+            }
+
         }
 
         //if (DoorInteractor.hitDoor == false)
