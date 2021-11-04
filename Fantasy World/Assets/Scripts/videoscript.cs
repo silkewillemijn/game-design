76e59f1f382a9c731a5a1c55ff6f79ae2036bace
silkewillemijn <s.w.franken@student.tue.nl>
2021-11-04 16:55:23 +0100

    i think i fixed the video

---
+++ Fantasy World/Assets/Scripts/videoscript.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+ 
+public class videoscript : MonoBehaviour
+{
+ 
+     VideoPlayer video;
+ 
+    void Awake()
+    {
+        video = GetComponent<VideoPlayer>();
+        video.Play();
+        video.loopPointReached += CheckOver;
+    }
+ 
+ 
+     void CheckOver(UnityEngine.Video.VideoPlayer vp)
+    {
+        SceneManager.LoadScene("Room");//the scene that you want to load after the video has ended.
+    }
+}
+ 
